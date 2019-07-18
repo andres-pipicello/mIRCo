5649287c6a48191ad75701c4b62f3911ec85e53b
Andres Pipicello <andres.pipicello@olx.com>
2019-07-18 16:42:39 -0300

    First version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io};
+use std::io::{Error, ErrorKind};
+use dns_lookup::get_hostname;
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    match &args[1..] {
+        [server, nick, real_name] => {
+            println!(
+                "NICK {}\nUSER {} {} {} :{}",
+                nick,
+                whoami::username(),
+                get_hostname().unwrap_or("localhost".to_string()),
+                server,
+                real_name
+            );
+            return Ok(());
+        }
+        _ => {
+            return Err(Error::new(ErrorKind::InvalidInput, "Usage: <server> <nick> <real-name>"));
+        }
+    }
 }
