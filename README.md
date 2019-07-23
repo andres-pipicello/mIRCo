48902fb371b8f3e1335a7cb450c77ba98d30d416
Andres Pipicello <andres.pipicello@olx.com>
2019-07-23 07:46:57 -0300

    Simple implementation of PING-PONG

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ The irc client
 ## Specs
 * https://tools.ietf.org/rfc/rfc1459.txt
 
+## Info
+* http://blog.initprogram.com/2010/10/14/a-quick-basic-primer-on-the-irc-protocol/
+* https://shiroyasha.svbtle.com/escape-sequences-a-quick-guide-1
+* https://github.com/krpors/hx/blob/develop/util.c#L213
+* https://mike42.me/blog/2018-06-make-better-cli-progress-bars-with-unicode-block-characters
+* http://www.irchelp.org/networks/servers/ircnet.html
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{env, io};
-use std::io::{Error, ErrorKind, Read, Write};
+use std::io::{BufRead, Error, ErrorKind, Write};
 use std::net::TcpStream;
+
+use bufstream::BufStream;
 use rustyline::error::ReadlineError;
 
 fn main() -> Result<(), io::Error> {
@@ -8,6 +10,7 @@ fn main() -> Result<(), io::Error> {
 
     match &args[1..] {
         [server, nick, real_name] => {
+            let server = server.clone();
             let nick_msg = format!("NICK {}\r\n", nick);
             let user_msg = format!("USER {} 0 * :{}\r\n",
                                    whoami::username(),
@@ -17,19 +20,39 @@ fn main() -> Result<(), io::Error> {
             let _join_handle = std::thread::spawn(move || {
                 stream.write_all(nick_msg.as_bytes()).unwrap();
                 stream.write_all(user_msg.as_bytes()).unwrap();
-                let mut stream = stream;
+                let mut stream = BufStream::new(stream);
                 loop {
-                    let mut buffer = [0u8; 512];
-                    let read = stream.read(&mut buffer).unwrap();
-                    println!("{}", String::from_utf8_lossy(&buffer[..read]));
+                    let mut line = String::new();
+                    let read_result = stream.read_line(&mut line);
+                    match read_result {
+                        Ok(0) => {
+                            eprintln!("connection to {} closed!!!", server);
+                            break;
+                        }
+                        Ok(_) => {
+                            let pos = line.find(|x: char| x.is_ascii_whitespace()).unwrap_or(line.len());
+                            match &line[0..pos] {
+                                "PING" => {
+                                    println!("PONG!!!");
+                                    let raw_rest = &line[pos..];
+                                    let rest_index = raw_rest.find(':').unwrap_or(raw_rest.len());
+                                    let message = &raw_rest[rest_index..];
+                                    stream.write_all(format!("PONG :{}\r\n", message).as_bytes()).unwrap();
+                                    stream.flush().unwrap();
+                                }
+                                _ => println!("unhandled message {}", line)
+                            }
+                        }
+                        Err(_) => eprintln!("{}", "unexpected error!!!")
+                    }
                 }
             });
 
             let mut rl = rustyline::Editor::<()>::new();
 
             loop {
-                let readline = rl.readline(">> ");
-                match readline {
+                let read_line = rl.readline(">> ");
+                match read_line {
                     Ok(line) => println!("Line: {:?}", line),
                     Err(ReadlineError::Interrupted) => {
                         println!("CTRL-C");
